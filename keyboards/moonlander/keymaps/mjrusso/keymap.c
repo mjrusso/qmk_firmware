ff70ff99158bb267bff0c8391da8a0eaafa613cf
Michael Russo <mjrusso@gmail.com>
2023-01-08 11:04:59 -0500

    Update Moonlander keymap to reflect physical hardware configuration.
    (I removed additional physical switches from the keyboard; they're no longer
    assigned keys in this keymap.)

--- keyboards/moonlander/keymaps/mjrusso/keymap.c
+++ keyboards/moonlander/keymaps/mjrusso/keymap.c
@@ -74,11 +74,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_moonlander(
         _______, _______, _______, _______,  _______,          _______, _______,                 _______, _______, _______,         _______,  _______, _______, _______,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,             KC_T,    _______,                 _______, KC_Y,    KC_U,            KC_I,    KC_O,    KC_P,    KC_GRAVE,
-        KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,             KC_G,    _______,                 _______, KC_H,    KC_J,            KC_K,    KC_L,    KC_QUOT, _______,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,             KC_B,                                      KC_N,    KC_M,            KC_COMM, KC_DOT,  KC_SLSH, _______,
-        KC_LCTL, _______, _______, KC_LALT,  KC_LCMD,          LA_MACRO,                         KC_ESC,           LT(NUM, KC_SPC), LA_FUN,   _______, _______, _______,
-                                             LT(NAV, KC_BSPC), LA_MOUSE,  KC_ESC,                LA_KEEB, _______, LT(SYM, KC_ENT)
+        KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,             KC_T,    _______,                 _______, KC_Y,    KC_U,            KC_I,    KC_O,    KC_P,     KC_GRAVE,
+        KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,             KC_G,    _______,                 _______, KC_H,    KC_J,            KC_K,    KC_L,    KC_QUOT,  _______,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,             KC_B,                                      KC_N,    KC_M,            KC_COMM, KC_DOT,  KC_SLSH,  _______,
+        _______, _______, _______, KC_LALT,  KC_LCMD,          KC_ESC,                           KC_ESC,           LT(NUM, KC_SPC), LA_FUN,   _______, _______, _______,
+                                             LT(NAV, KC_BSPC), _______, _______,                 _______, _______, LT(SYM, KC_ENT)
     ),
 
    [NAV] = LAYOUT_moonlander(
