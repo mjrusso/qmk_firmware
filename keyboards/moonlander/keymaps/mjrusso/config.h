a25129041ab9b806cb47482d64bd4deee514606c
Michael Russo <mjrusso@gmail.com>
2023-01-09 09:21:35 -0500

    Enable TAPPING_FORCE_HOLD.

--- keyboards/moonlander/keymaps/mjrusso/config.h
+++ keyboards/moonlander/keymaps/mjrusso/config.h
@@ -20,6 +20,11 @@
 
 #define ORYX_CONFIGURATOR
 
+// Activate the hold function, instead of repeating the tapping function, when
+// holding a key immediately after tapping.
+// See: https://docs.qmk.fm/#/tap_hold?id=tapping-force-hold
+#define TAPPING_FORCE_HOLD
+
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
